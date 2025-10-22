0835e431a1501fb5f3be586ad5257e6fca53f877
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2025-10-22 04:22:40 -0800

    some more change

--- src/app.ts
+++ src/app.ts
@@ -11,16 +11,9 @@ const app: Application = express();
 app.use(express.json());
 // CORS configuration
 const allowedOrigins = ['http://localhost:5173', 'https://www.ssnmax.store'];
+
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      return callback(new Error('CORS policy: Access denied'), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: allowedOrigins,
   credentials: true,
 }));
 
